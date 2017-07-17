8f30eb4ddb44c4c30bfa59c5e5b0d8c93b7a9702
Esti Shay and Paul Guevarra <esti@>
2017-07-17 15:44:47 -0700

    allow user to remove completed tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 //business logic
-
-
+//
 //UI logic
 $(document).ready(function() {
   $("form#adding-tasks").submit(function(event) {
     event.preventDefault();
 
     var inputtedTask = $("input#new-task").val();
+    // var newTask = new Task(inputtedTask);
+    // $("ul").append("<li>" + inputtedTask + "</li>");
+    $("ul").append("<li><label><input type='checkbox' name='todos'>" + inputtedTask + "</label></li>")
 
-    $("ul").append("<li>" + inputtedTask + "</li>");
+    $("input#new-task").val("");
 
   });
+
+  $("form#our-tasks").submit(function(event) {
+    event.preventDefault();
+    // var checked = $("input:checkbox[name='todos']:checked").val();
+    //  $(".checkbox").last().remove();
+     $("input:checkbox[name=todos]:checked").parent().parent().remove();
+
+
+    // var checked = $("input:checkbox[name=todos]:checked").val();
+    //  $(".checkbox").last().remove(checked);
+  });
+
 });
